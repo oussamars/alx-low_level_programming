4d116abf089a3e9b89691c796cb80fea43794987
oussama <oussamabousselham2020@gmail.com>
2023-03-16 12:26:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line with puts function
-*
-* Return : 0 (Success)
-*/
-int main(void) {
+ * main - A program that prints a line with puts function
+ * Return : 0 (Succes)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
