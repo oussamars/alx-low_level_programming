31069778959321679c68b8c2a7b3c58c90c20b0b
oussama <oussamabousselham2020@gmail.com>
2023-03-21 10:59:21 -0700

    alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets ten times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (letter = 97; letter < 123; letter++)
+		{
+			_putchar(letter);
+		i++;
+		}
+		_putchar('n');
+	}
+}
