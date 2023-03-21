2d3e170009e6aa8e95059c4586d7195a750a9a0d
oussama <oussamabousselham2020@gmail.com>
2023-03-21 11:28:32 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check if the letter is lowercase.
+ * @c: The character to be checked
+ *
+ * Return: 1 for lowercase and 0 for anything else
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (1);
+	}
+}
