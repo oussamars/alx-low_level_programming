66e7ae37b298aa10674d2fea1687336d015e82fd
oussama <oussamabousselham2020@gmail.com>
2023-03-22 03:29:57 -0700

    4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -14,15 +14,16 @@ int main(void)
 	k = 2;
 	sum = 0;
 
-	for (i = 1; i <= 33; ++i)
+	while (j < 4000000)
 	{
-		if (j < 4000000 && (j % 2) == 0)
+		if(j % 2 ==0)
 		{
 			sum = sum + j;
-			j = k;
-			k = next;
 		}
+		j = k;
+		k = next;
+		
+	}
 		printf("%lu\n", sum);
 		return (0);
-	}
 }
