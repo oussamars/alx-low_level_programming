10248b205c208254e9cb8f43b9aeb6b639522a89
oussama <oussamabousselham2020@gmail.com>
2023-03-23 11:03:58 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,7 +6,7 @@
  */
 void print_diagonal(int n)
 {
-	int x;
+	int x, y;
 
 	if (n <= 0)
 	{
@@ -16,8 +16,12 @@ void print_diagonal(int n)
 	{
 		for (x = 0; x < n; x++)
 		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
 			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
