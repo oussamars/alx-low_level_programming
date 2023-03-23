144b1c3a29887a2cd298391869135575e70bd888
oussama <oussamabousselham2020@gmail.com>
2023-03-23 09:10:25 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,20 @@
 
 /**
  * print_most_numbers - prints the numbers form 0 to 9 without 2 and 4
- * Return: Nothing
+ * Return: The numbers from 0 to 9
  */
+
 void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 9; i++)
 	{
-		if (i != 2 || i != 4)
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
 		{
 			_putchar(i + '0');
 		}
