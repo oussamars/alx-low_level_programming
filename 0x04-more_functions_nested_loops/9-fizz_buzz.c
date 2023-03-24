db901cee0bbfafe468ebef43044923c9ea29128f
oussama <oussamabousselham2020@gmail.com>
2023-03-24 07:12:22 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("fizzbuzz ");
+		}
+		else if ((i % 3) == 0)
 		{
 			printf("fizz ");
 		}
 		else if ((i % 5) == 0)
 		{
 			printf("buzz ");
-		}
-		else if ((i % 5) == 0 && (i % 3) == 0)
-		{
-			printf("fizzbuzz");
 		}
 		else
 		{
