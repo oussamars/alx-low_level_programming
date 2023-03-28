4a7f3bc22d898c5d68a2fb49fdecefff7e668581
oussama <oussamabousselham2020@gmail.com>
2023-03-27 17:44:43 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,26 +3,35 @@
 #include <string.h>
 #include <time.h>
 
+#define MAX_ATTEMPTS 1000000
+
 int main(void) {
   char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]|:;”’<>,.?/~`";
   int valid_chars_len = sizeof(valid_chars) - 1;
   int password_len = 15;
   char *password = malloc(password_len + 1);
-  int i;
+  int i, attempts = 0;
 
   srand(time(NULL));
 
-  do {
+  while (attempts < MAX_ATTEMPTS) {
     for (i = 0; i < password_len; i++) {
       int random_index = rand() % valid_chars_len;
       password[i] = valid_chars[random_index];
     }
     password[password_len] = '\0';
-  } while (strcmp(password, "Congratulations!") != 0);
 
-  printf("%s", password);
+    if (strcmp(password, "Congratulations!") == 0) {
+      printf("%s", password);
+      free(password);
+      return 0;
+    }
 
+    attempts++;
+  }
+
+  fprintf(stderr, "Failed to generate a valid password after %d attempts\n", attempts);
   free(password);
 
-  return 0;
+  return 1;
 }
