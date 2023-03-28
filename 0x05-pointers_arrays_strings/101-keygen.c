d2191d424bbd597cd73d86ce6bcbd8ebc83f1ae6
oussama <oussamabousselham2020@gmail.com>
2023-03-27 17:36:57 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 int main(void) {
   char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]|:;”’<>,.?/~`";
   int valid_chars_len = sizeof(valid_chars) - 1;
-  int password_len = 10;
+  int password_len = 15;
   char *password = malloc(password_len + 1);
   int i;
 
   srand(time(NULL));
 
-  for (i = 0; i < password_len; i++) {
-    int random_index = rand() % valid_chars_len;
-    password[i] = valid_chars[random_index];
-  }
-  password[password_len] = '\0';
+  do {
+    for (i = 0; i < password_len; i++) {
+      int random_index = rand() % valid_chars_len;
+      password[i] = valid_chars[random_index];
+    }
+    password[password_len] = '\0';
+  } while (strcmp(password, "Congratulations!") != 0);
 
   printf("%s", password);
 
