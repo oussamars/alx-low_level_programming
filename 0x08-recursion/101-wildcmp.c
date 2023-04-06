7d5bd7045feec9f1a5778038deb6fa0a6a853eb5
oussama <oussamabousselham2020@gmail.com>
2023-04-06 07:28:55 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,7 +20,7 @@ int wildcmp(char *s1, char *s2)
 		}
 		else
 		{
-			return (wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1));
+			return ((wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1)));
 		}
 	}
 	if (*s1 == *s2)
