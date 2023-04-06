20b6a72681f9134130e92fad53d8b04c4a35820a
oussama <oussamabousselham2020@gmail.com>
2023-04-06 07:26:37 -0700

    wildcmp

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: the string to check
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	if (len <= 1)
+	{
+		return (1);
+	}
+	else if (s[0] != s[len - 1])
+	{
+		return (0);
+	}
+	else
+	{
+		s[len -1] = '\0';
+		return (is_palindrome(s + 1));
+	}
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings
+ * @s1: the first string to compare
+ * @s2: the second string to compare
+ *
+ * Return: 1 if they are identical
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		else
+		{
+			return (wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1));
+		}
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
