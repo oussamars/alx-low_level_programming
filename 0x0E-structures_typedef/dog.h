903d1713949dbfbde7929565aff096f2062519a1
oussama <oussamabousselham2020@gmail.com>
2023-04-17 07:38:13 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure that holds dog info
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner's name
+ *
+ * Description: a structure that holds basic info about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
