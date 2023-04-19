2aff5d4044d86d2478cc42431e3e3707104a1163
oussama <oussamabousselham2020@gmail.com>
2023-04-19 09:08:39 -0700

    a

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: function pointer
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
