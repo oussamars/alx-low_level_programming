519a29245819e9a396de8f0a01fe3773b25c5ed1
oussama <oussamabousselham2020@gmail.com>
2023-04-27 15:19:05 -0700

    printing before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * before_main - function executed before main
+ * Return: void
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
