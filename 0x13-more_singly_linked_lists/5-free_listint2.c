7d9dbb2226d8289fc009303033bf25767830f9c8
oussama <oussamabousselham2020@gmail.com>
2023-04-29 12:20:56 -0700

    free listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *current;
 
 	if (head == NULL)
 		return;
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		temp = *head;
+		current = *head;
 		*head = (*head)->next;
-		free(temp);
+		free(current);
 	}
+	*head = NULL;
 }
