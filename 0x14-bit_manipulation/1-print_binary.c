4b4a049a60246a2a226feef2ac95d7e16b95b69d
oussama <oussamabousselham2020@gmail.com>
2023-05-04 09:03:03 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,22 +5,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int d = 1;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (d <= n)
-		d <<= 1;
-	d >>= 1;
-	while (d > 0)
-	{
-		if (n & d)
-			_putchar('1');
-		else
-			_putchar('0');
-		d >>= 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) ? '1' : '0');
 }
