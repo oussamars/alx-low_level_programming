cf5e880a8cc111bcd013f8b4a0d82470403426da
oussama <oussamabousselham2020@gmail.com>
2023-05-08 03:39:37 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,6 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
-		return (-1);
-	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 			len++;
